60d238d6112d9d70cf99508cc5b0670003718f88
Jack Watson <32821110+JackWatson06@users.noreply.github.com>
2021-05-10 03:24:11 -0400

    Git Ignore, HTML Changes, and Link Tracking

--- links/employee.php
+++ links/employee.php
@@ -1,4 +1,25 @@
 <?php
     header("Location: https://americanlaborcompany.com/employee");
+
+    $filePath = getcwd() . "/employee_count.txt";
+    $file = @fopen($filePath, "c+");
+
+    //File loaded
+    if($file)
+    {
+        $data = fread($file, filesize($filePath));
+
+        $data = intval($data);
+        $data++;
+
+        // Delete then flush.
+        ftruncate($file, 0);
+        rewind($file);
+        fwrite($file, $data);
+        fflush($file);
+
+        $file = fclose($file);
+    }   
+
     exit();
 ?>
